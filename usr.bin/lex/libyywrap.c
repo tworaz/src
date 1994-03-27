1f58cc6a466e958da68735b335807511351820d3
cvs <cvs>
1994-03-27 09:27:00 +0000

    Creating branch agc_pkgs

--- usr.bin/lex/libyywrap.c
+++
@@ -1,8 +0,0 @@
-/* libyywrap - flex run-time support library "yywrap" function */
-
-/* $Header: /home/joerg/repo/netbsd/src/usr.bin/lex/libyywrap.c,v 1.1 1994/02/04 19:15:30 jtc Exp $ */
-
-int yywrap()
-	{
-	return 1;
-	}
