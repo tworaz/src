3ca6b7d1afa24d44678e012d56ec749c1c37bcef
pk <pk>
1995-06-05 19:44:52 +0000

    Prototypes (PR#1102).

--- usr.bin/lex/libyywrap.c
+++ usr.bin/lex/libyywrap.c
@@ -1,6 +1,10 @@
 /* libyywrap - flex run-time support library "yywrap" function */
 
-/* $Header: /home/joerg/repo/netbsd/src/usr.bin/lex/libyywrap.c,v 1.2 1995/05/05 05:35:32 jtc Exp $ */
+/* $Header: /home/joerg/repo/netbsd/src/usr.bin/lex/libyywrap.c,v 1.3 1995/06/05 19:44:55 pk Exp $ */
+
+#include <sys/cdefs.h>
+
+int yywrap __P((void));
 
 int yywrap()
 	{
