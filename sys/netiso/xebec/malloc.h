1f8d9bb0d1a3e3d4f53096a4d17a6a56121aca63
fvdl <fvdl>
1998-03-01 02:09:34 +0000

    Import 4.4BSD-Lite for reference

---
+++ sys/netiso/xebec/malloc.h
@@ -0,0 +1,4 @@
+/* $Header: /home/joerg/repo/netbsd/src/sys/netiso/xebec/malloc.h,v 1.1.1.1 1998/03/01 02:10:30 fvdl Exp $ */
+/* $Source: /home/joerg/repo/netbsd/src/sys/netiso/xebec/malloc.h,v $ */
+
+char *Malloc();
