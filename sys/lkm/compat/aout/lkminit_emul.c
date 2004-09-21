fd8da688228459ac5a609fe829a4c6a536836508
skrll <skrll>
2004-09-21 13:32:19 +0000

    Fix the sync with head I botched.

--- sys/lkm/compat/aout/lkminit_emul.c
+++ sys/lkm/compat/aout/lkminit_emul.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lkminit_emul.c,v 1.5 2001/12/08 00:37:07 thorpej Exp $ */
+/* $NetBSD: lkminit_emul.c,v 1.5.16.2 2004/09/21 13:36:23 skrll Exp $ */
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: lkminit_emul.c,v 1.5 2001/12/08 00:37:07 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: lkminit_emul.c,v 1.5.16.2 2004/09/21 13:36:23 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/ioctl.h>
